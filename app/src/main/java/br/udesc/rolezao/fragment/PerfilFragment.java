09c9ef0929217d009cb0539902e6894340159249
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2019-09-27 20:18:28 -0300

    PerfilFragment created

---
+++ app/src/main/java/br/udesc/rolezao/fragment/PerfilFragment.java
@@ -0,0 +1,52 @@
+package br.udesc.rolezao.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.udesc.rolezao.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link PerfilFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link PerfilFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PerfilFragment extends Fragment {
+
+
+    public PerfilFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_perfil, container, false);
+    }
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
