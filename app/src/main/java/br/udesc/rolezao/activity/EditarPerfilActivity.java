6011fc95d4af07c4202266dcca0715bf8ff3617d
Vinícius Vieira <vinny.vieira99@hotmail.com>
2019-10-03 17:11:15 -0300

    Layout do perfil e alteração de perfil

---
+++ app/src/main/java/br/udesc/rolezao/activity/EditarPerfilActivity.java
@@ -0,0 +1,25 @@
+package br.udesc.rolezao.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import br.udesc.rolezao.R;
+
+public class EditarPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_perfil);
+
+        // Configura Toolbar
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Editar Perfil");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //Diz que é uma janela para voltar
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp); //Trocar icone
+    }
+}
--- app/src/main/java/br/udesc/rolezao/fragment/PerfilFragment.java
+++ app/src/main/java/br/udesc/rolezao/fragment/PerfilFragment.java
@@ -1,6 +1,7 @@
 package br.udesc.rolezao.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import br.udesc.rolezao.R;
+import br.udesc.rolezao.activity.EditarPerfilActivity;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PerfilFragment extends Fragment {
+
+    private TextView nomeUsuario, cidadeUsuario, nivelUsuario, conquistasUsuario, dinheiroUsuario;
+    private ProgressBar progressBarUsuario;
+    private CircleImageView fotoUsuario;
+    private FloatingActionButton buttonEditarPerfil;
 
 
     public PerfilFragment() {
@@ -26,7 +39,29 @@ public class PerfilFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        //Configuração dos componentes
+        nomeUsuario = view.findViewById(R.id.nomeUsuario);
+        cidadeUsuario = view.findViewById(R.id.cidadeUsuario);
+        nivelUsuario = view.findViewById(R.id.nivelUsuario);
+        conquistasUsuario = view.findViewById(R.id.conquistasUsuario);
+        dinheiroUsuario = view.findViewById(R.id.dinheiroUsuario);
+        progressBarUsuario = view.findViewById(R.id.progressBarUsuario);
+        fotoUsuario = view.findViewById(R.id.fotoUsuario);
+        buttonEditarPerfil = view.findViewById(R.id.buttonEditarPerfil);
+
+        //Abrir edição do perfil
+
+        buttonEditarPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i  = new Intent(getActivity(), EditarPerfilActivity.class);
+                startActivity(i);
+            }
+        });
+
+        return view;
     }
 
 }
