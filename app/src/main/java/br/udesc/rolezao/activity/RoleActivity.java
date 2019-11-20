eec8e3891bac4df35e4be6c2d0de1ec21c769505
ViniciusTomeVieira <vinny.vieira99@hotmail.com>
2019-11-20 20:15:03 -0300

    RoleActivity created

---
+++ app/src/main/java/br/udesc/rolezao/activity/RoleActivity.java
@@ -0,0 +1,15 @@
+package br.udesc.rolezao.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import br.udesc.rolezao.R;
+
+import android.os.Bundle;
+
+public class RoleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_role);
+    }
+}
